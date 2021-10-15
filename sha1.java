397225ce68c29c00bfe66feec4e9360fd017b221
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2021-10-15 12:48:46 +0530

    Update sha1.java

--- sha1.java
+++ sha1.java
@@ -42,3 +42,17 @@ public class sha1
  return buf.toString();
  }
 }
+/*
+Message digest object info:
+-----------------
+Algorithm=SHA1
+Provider=SUN version 11
+ToString=SHA1 Message Digest from SUN, <initialized>
+
+
+SHA1("")=DA39A3EE5E6B4B0D3255BFEF95601890AFD80709
+
+SHA1("abc")=A9993E364706816ABA3E25717850C26C9CD0D89D
+
+SHA1("abcdefghijklmnopqrstuvwxyz")=32D10C7B8CF96570CA04CE37F2A19D84240D3A89
+*/
